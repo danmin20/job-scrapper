138f87b8e33c5450d127bb7fc9b791218b65f3c2
Lee Jeong Min <danmin20@khu.ac.kr>
2020-03-17 17:42:08 +0900

    file download

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"github.com/danmin20/job-scrapper/scrapper"
 	"github.com/labstack/echo"
+	"os"
 	"strings"
 )
+
+const fileName string = "jobs.csv"
 
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	return nil
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
